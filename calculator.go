a89637d4619ab86470cd31666437846ab7be0c85
Maksim <maximus.kor53@gmail.com>
2024-08-17 17:04:29 +0300

    backward-breaking

--- calculator.go
+++ calculator.go
@@ -1,7 +1,13 @@
 // Package ch10 provides methods for calculus operations
 package ch10
 
-// Add calculate sum of two int values and returns it
-func Add(a, b int) int {
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add calculate sum of two values of type Number and returns it
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/malaxitlmax/learninggo10
 
 go 1.22.5
+
+require golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
